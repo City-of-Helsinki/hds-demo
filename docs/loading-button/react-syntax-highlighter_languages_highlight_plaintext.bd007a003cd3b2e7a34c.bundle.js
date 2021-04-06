49be4c089bdba8ff77c8c09f535dfc9b46fb44f3
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-04-06 15:50:32 +0300

    Remove unused examples

--- docs/loading-button/react-syntax-highlighter_languages_highlight_plaintext.bd007a003cd3b2e7a34c.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2063:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.bd007a003cd3b2e7a34c.bundle.js.map
