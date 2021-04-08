d4c97af1109dfdc4643e280522f5154ef0464745
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-04-08 13:36:06 +0300

    Add new dialog version

--- docs/dialog/react-syntax-highlighter_languages_highlight_juliaRepl.59651518dcf8ed3c813a.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[85],{2039:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_juliaRepl.59651518dcf8ed3c813a.bundle.js.map
--- docs/dialog/react-syntax-highlighter_languages_highlight_vbscriptHtml.59651518dcf8ed3c813a.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[169],{2127:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.59651518dcf8ed3c813a.bundle.js.map
