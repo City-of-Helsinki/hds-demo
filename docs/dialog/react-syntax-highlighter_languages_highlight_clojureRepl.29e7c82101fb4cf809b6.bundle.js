38f3bbe6827c8e864fc7c2d6f54fe4c4f7e24941
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-04-21 11:52:04 +0300

    Remove dialog example

--- docs/dialog/react-syntax-highlighter_languages_highlight_clojureRepl.29e7c82101fb4cf809b6.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{1982:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_clojureRepl.29e7c82101fb4cf809b6.bundle.js.map
