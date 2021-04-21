5f3e4782e34925b879d03d9e1473c7bfb56df040
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-04-21 11:51:20 +0300

    Add new status-label version

---
+++ docs/status-label/react-syntax-highlighter_languages_highlight_plaintext.6f269839d39340f2731f.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2081:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.6f269839d39340f2731f.bundle.js.map
