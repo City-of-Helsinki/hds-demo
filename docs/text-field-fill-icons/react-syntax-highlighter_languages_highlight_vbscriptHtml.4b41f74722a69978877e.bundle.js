a718c64bf2b430d62a8687d8aeccdf978b8d8d37
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-05-04 11:22:43 +0300

    Remove unused examples

--- docs/text-field-fill-icons/react-syntax-highlighter_languages_highlight_vbscriptHtml.4b41f74722a69978877e.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[169],{2130:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.4b41f74722a69978877e.bundle.js.map
