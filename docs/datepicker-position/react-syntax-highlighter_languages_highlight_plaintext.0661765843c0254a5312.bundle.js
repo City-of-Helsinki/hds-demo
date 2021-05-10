142ab3799b3c4cdc742cc0ec604bba15108e6820
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-05-10 15:05:38 +0300

    Add new datepicker version

--- docs/datepicker-position/react-syntax-highlighter_languages_highlight_plaintext.0661765843c0254a5312.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2081:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.0661765843c0254a5312.bundle.js.map
--- docs/datepicker-position/service-worker.js
+++ docs/datepicker-position/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a71d7c4b22ee89f6d2ef91ca11f4bd27.js"
+  "precache-manifest.9bc8316ba29fa1fce397c795451e985f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
