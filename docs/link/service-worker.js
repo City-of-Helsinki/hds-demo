e81774d6c48ee32d0a3c0a01d2a18aae45f0f773
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-06-21 15:39:03 +0300

    Add new link version

--- docs/link/service-worker.js
+++ docs/link/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.affbccdefe3b532c536536cc80e9789f.js"
+  "precache-manifest.e91680298ccd1a72335cb7424b579bf7.js"
 );
 
 self.addEventListener('message', (event) => {
