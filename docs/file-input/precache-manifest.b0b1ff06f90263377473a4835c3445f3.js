3065865cf3646e602d8c886917241a5a78071cb8
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-08-17 15:14:25 +0300

    Add new file-input version

---
+++ docs/file-input/precache-manifest.b0b1ff06f90263377473a4835c3445f3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319dc95978ed5db0132b",
+    "url": "0.2dced975.iframe.bundle.js"
+  },
+  {
+    "revision": "80ebaf01f7b6c5327eba",
+    "url": "4.921f962f.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.921f962f.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "d841d90e17c874ef37c1",
+    "url": "5.5103f154.iframe.bundle.js"
+  },
+  {
+    "revision": "2cef647001cd2bbea7a7",
+    "url": "6.27c3d648.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.27c3d648.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd85565c3703e5bd82df",
+    "url": "7.ef645950.iframe.bundle.js"
+  },
+  {
+    "revision": "6e42ba94c8358e639f45c178b84aa748",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "31d48303e59f02c98310",
+    "url": "main.1e7d36db.iframe.bundle.js"
+  },
+  {
+    "revision": "0f91f4e31082c29e9614",
+    "url": "runtime~main.fb80ef4e.iframe.bundle.js"
+  },
+  {
+    "revision": "31d48303e59f02c98310",
+    "url": "static/css/main.5cb2f5fb.chunk.css"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "40642d679edee0636b89",
+    "url": "vendors~main.f853886b.iframe.bundle.js"
+  },
+  {
+    "revision": "0b5b9cb2b6feb6f71cace5c62a6bc331",
+    "url": "vendors~main.f853886b.iframe.bundle.js.LICENSE.txt"
+  }
+]);
--- docs/file-input/service-worker.js
+++ docs/file-input/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.048bc4a2497deb15799fba06eb9cd979.js"
+  "precache-manifest.b0b1ff06f90263377473a4835c3445f3.js"
 );
 
 self.addEventListener('message', (event) => {
