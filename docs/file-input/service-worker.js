7e926b64151e95a4c80cae48089d72589501a877
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-09-01 17:47:29 +0300

    Add new file-input version

--- docs/file-input/service-worker.js
+++ docs/file-input/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a0badd79ecfd29cc15c2390b0ba32dc4.js"
+  "precache-manifest.934de80d823eafd6f160e03a5cdde1d4.js"
 );
 
 self.addEventListener('message', (event) => {
