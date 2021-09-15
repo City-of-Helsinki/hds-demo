53d6fb8ed68f10a50333f4ff1cfb1f1e1e83ee30
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-09-15 14:27:34 +0300

    Add initial shopping cart icon

---
+++ docs/shopping-cart-icon/precache-manifest.43b547bfeeca14bc359534b94f361a8b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374ba923cf4cd4ef9ea4",
+    "url": "0.97fc973c.iframe.bundle.js"
+  },
+  {
+    "revision": "34621278002dd4e627be",
+    "url": "4.4e2ef301.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.4e2ef301.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d0e471771cc5f20e90",
+    "url": "5.df4f1a1c.iframe.bundle.js"
+  },
+  {
+    "revision": "649cb760ceaeec1e1ffa",
+    "url": "6.f762401a.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.f762401a.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f90ba5466a142e885ae",
+    "url": "7.11929316.iframe.bundle.js"
+  },
+  {
+    "revision": "92f7121712c9d54cee811291b39dd420",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "0bfd2edb3bd87faf9517",
+    "url": "main.19a8dc5d.iframe.bundle.js"
+  },
+  {
+    "revision": "05e6e1563e678b331194",
+    "url": "runtime~main.f5bf8320.iframe.bundle.js"
+  },
+  {
+    "revision": "0bfd2edb3bd87faf9517",
+    "url": "static/css/main.8d31e468.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "0bd335f51d2abba1d899",
+    "url": "vendors~main.b1b71fcf.iframe.bundle.js"
+  },
+  {
+    "revision": "0b5b9cb2b6feb6f71cace5c62a6bc331",
+    "url": "vendors~main.b1b71fcf.iframe.bundle.js.LICENSE.txt"
+  }
+]);
