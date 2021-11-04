ec3ead562b6fdb2d0d699ff28f6db0c199c2ec63
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-11-04 16:51:25 +0200

    Add initial datepicker keyboard navigation bugfix version

---
+++ docs/datepicker-keyboard-navigation-fix/precache-manifest.5b1822984eabe003135058c7b0bb396c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e193b79d41116fe9d799",
+    "url": "0.48ce0ef0.iframe.bundle.js"
+  },
+  {
+    "revision": "2c5b21a38ba08c5847b3",
+    "url": "4.803dbd81.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.803dbd81.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fd2b5b5763d65317be",
+    "url": "5.e7b1833d.iframe.bundle.js"
+  },
+  {
+    "revision": "57916802f6ee96b1000e",
+    "url": "6.194a930d.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.194a930d.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ecd160383e4d2c3a05",
+    "url": "7.194e93fb.iframe.bundle.js"
+  },
+  {
+    "revision": "72789b185890299a9ff4b5f7098afbbb",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "e602a4d96514dcafbfd9",
+    "url": "main.60a6bc61.iframe.bundle.js"
+  },
+  {
+    "revision": "6cb5a48ce968d173b029",
+    "url": "runtime~main.5053bbbb.iframe.bundle.js"
+  },
+  {
+    "revision": "e602a4d96514dcafbfd9",
+    "url": "static/css/main.d1f9297e.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "7b3005fa6eaba3d294c9",
+    "url": "vendors~main.59ffbc9f.iframe.bundle.js"
+  },
+  {
+    "revision": "0b5b9cb2b6feb6f71cace5c62a6bc331",
+    "url": "vendors~main.59ffbc9f.iframe.bundle.js.LICENSE.txt"
+  }
+]);
