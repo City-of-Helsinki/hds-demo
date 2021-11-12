fa1cf892ecb3661da79eb0d9dc6a31ff1c20cd51
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-11-12 09:17:37 +0200

    Add new dateinput version

---
+++ docs/dateinput-exludedates/precache-manifest.175afeb3bc45e32df59120c54de01288.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df261d9a120ea63ef09",
+    "url": "0.0feb1446.iframe.bundle.js"
+  },
+  {
+    "revision": "bbfbcb781d21798dff81",
+    "url": "4.67144dcd.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.67144dcd.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5d4f2f042176d30533",
+    "url": "5.1f7fa069.iframe.bundle.js"
+  },
+  {
+    "revision": "19c9a1fe26d3d62cae9c",
+    "url": "6.104793d0.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.104793d0.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb5851631e395bffb6a",
+    "url": "7.800894c0.iframe.bundle.js"
+  },
+  {
+    "revision": "1eb238b9e4e75d1340128aa77b4f9719",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "dc17e04e9616989b7cdd",
+    "url": "main.dbf61042.iframe.bundle.js"
+  },
+  {
+    "revision": "c255be0512a929726474",
+    "url": "runtime~main.fa4628cc.iframe.bundle.js"
+  },
+  {
+    "revision": "dc17e04e9616989b7cdd",
+    "url": "static/css/main.d1f9297e.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "4ca991d04418eb72e9cd",
+    "url": "vendors~main.73ef8378.iframe.bundle.js"
+  },
+  {
+    "revision": "0b5b9cb2b6feb6f71cace5c62a6bc331",
+    "url": "vendors~main.73ef8378.iframe.bundle.js.LICENSE.txt"
+  }
+]);
--- docs/dateinput-exludedates/service-worker.js
+++ docs/dateinput-exludedates/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.23c470ebf98a01b785bf3fb88e41870e.js"
+  "precache-manifest.175afeb3bc45e32df59120c54de01288.js"
 );
 
 self.addEventListener('message', (event) => {
