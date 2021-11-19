655329018415d8c9a49a509a2e0d0bed0cada88d
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-11-19 18:29:12 +0200

    Add new cookie consent version

--- docs/cookie-consent/service-worker.js
+++ docs/cookie-consent/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.ede142d6f62976486919e29108bf3290.js"
+  "precache-manifest.a7aa9010033e11bbb9fc040f97bd4f8a.js"
 );
 
 self.addEventListener('message', (event) => {
