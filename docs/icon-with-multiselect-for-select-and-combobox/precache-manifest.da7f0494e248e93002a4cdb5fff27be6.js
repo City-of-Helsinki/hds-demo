c52359dc04a57717c71d6fdb48c5988816d2b41e
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-12-01 16:31:15 +0200

    Add multiselect with icon support also to Select component

---
+++ docs/icon-with-multiselect-for-select-and-combobox/precache-manifest.da7f0494e248e93002a4cdb5fff27be6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29d00318b553ee693d3",
+    "url": "0.ee8d3068.iframe.bundle.js"
+  },
+  {
+    "revision": "e8990b28a7d1c4098a4e",
+    "url": "4.5b3787af.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.5b3787af.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb0e0af6f6e99beaa01",
+    "url": "5.3d86d762.iframe.bundle.js"
+  },
+  {
+    "revision": "65f3177b815e487e4d23",
+    "url": "6.34f53ea1.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.34f53ea1.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "051ae8d17d82887f26d4",
+    "url": "7.f9775329.iframe.bundle.js"
+  },
+  {
+    "revision": "18b13677962ef1f74f49ba33a89e7d07",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "53b23905ab78d01d23fc",
+    "url": "main.0d40ed11.iframe.bundle.js"
+  },
+  {
+    "revision": "b071383b0a94e2dd41b3",
+    "url": "runtime~main.38d6be17.iframe.bundle.js"
+  },
+  {
+    "revision": "53b23905ab78d01d23fc",
+    "url": "static/css/main.77510236.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "0828a02f31157ec6fb6c",
+    "url": "vendors~main.92cd7097.iframe.bundle.js"
+  },
+  {
+    "revision": "a50688c9074bda7d96902f536e7455ae",
+    "url": "vendors~main.92cd7097.iframe.bundle.js.LICENSE.txt"
+  }
+]);
