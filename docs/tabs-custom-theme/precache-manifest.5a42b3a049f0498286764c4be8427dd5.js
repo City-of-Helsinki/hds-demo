94fde05f904151f82ace3030bbdfe193c6cda71b
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-12-02 15:22:42 +0200

    Add version of tabs with custom theme

---
+++ docs/tabs-custom-theme/precache-manifest.5a42b3a049f0498286764c4be8427dd5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8d9024fec8a4dce434",
+    "url": "0.6925cf07.iframe.bundle.js"
+  },
+  {
+    "revision": "38640717cb56e391db5c",
+    "url": "4.8efec254.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.8efec254.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9970e3d6fd2e5f31494",
+    "url": "5.c5e1705b.iframe.bundle.js"
+  },
+  {
+    "revision": "846ce600b171f3e13248",
+    "url": "6.f4959b9d.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.f4959b9d.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "051ae8d17d82887f26d4",
+    "url": "7.f9775329.iframe.bundle.js"
+  },
+  {
+    "revision": "d97bd5afeff734ba81666a03018c82ec",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "b65e39885049ef8753a7",
+    "url": "main.ef9183fe.iframe.bundle.js"
+  },
+  {
+    "revision": "e004f250bee4eee5feec",
+    "url": "runtime~main.76139664.iframe.bundle.js"
+  },
+  {
+    "revision": "b65e39885049ef8753a7",
+    "url": "static/css/main.6b4f014a.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "b498835710e12c046333",
+    "url": "vendors~main.544bd3cc.iframe.bundle.js"
+  },
+  {
+    "revision": "a50688c9074bda7d96902f536e7455ae",
+    "url": "vendors~main.544bd3cc.iframe.bundle.js.LICENSE.txt"
+  }
+]);
