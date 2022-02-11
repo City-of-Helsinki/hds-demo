7d341199a8ae40a307cfa1ba0ab92f688d9d5b38
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-02-11 16:03:31 +0200

    Add storybook v6.4.18 demo

---
+++ docs/storybook-v6.4.18/precache-manifest.7fb1dd7836e84b204f161133012d9beb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5947f2341a587b8b99af",
+    "url": "0.477afa00.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "0.477afa00.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89ff52de00f9e5c4386",
+    "url": "1.02fab0d3.iframe.bundle.js"
+  },
+  {
+    "revision": "76eab847940e9c5c3e3f",
+    "url": "2.ae39fcb6.iframe.bundle.js"
+  },
+  {
+    "revision": "7308544f8bb104a5d8e9",
+    "url": "6.ac337c5b.iframe.bundle.js"
+  },
+  {
+    "revision": "6902c4d16ff44baa6c7a",
+    "url": "7.5773f2ac.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "7.5773f2ac.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "26dc4c067c2ea6d33390",
+    "url": "8.c1f550d2.iframe.bundle.js"
+  },
+  {
+    "revision": "55ebdedbd4ee3a9e93f7110c5f0574d0",
+    "url": "8.c1f550d2.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "644f907e34cc67b7aac5",
+    "url": "9.04b35eeb.iframe.bundle.js"
+  },
+  {
+    "revision": "566e41a8ce7197cb4727eb4b885a9ba2",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "0fe907227d63ef924fc8",
+    "url": "main.e4befd66.iframe.bundle.js"
+  },
+  {
+    "revision": "0036e1bd10171352694e",
+    "url": "runtime~main.b8906f32.iframe.bundle.js"
+  },
+  {
+    "revision": "0fe907227d63ef924fc8",
+    "url": "static/css/main.f421a9b0.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "f8ccf98b48c599bc50f4",
+    "url": "vendors~main.9406e125.iframe.bundle.js"
+  },
+  {
+    "revision": "0e44f97f3f6f97051c82b4dcdf8935c4",
+    "url": "vendors~main.9406e125.iframe.bundle.js.LICENSE.txt"
+  }
+]);
