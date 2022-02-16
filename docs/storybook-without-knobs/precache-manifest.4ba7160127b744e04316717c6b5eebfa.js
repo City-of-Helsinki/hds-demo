c7730101a78ae074c935c695faf544060f88cf75
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-02-16 17:53:36 +0200

    Add new storybook-without-knobs example version

---
+++ docs/storybook-without-knobs/precache-manifest.4ba7160127b744e04316717c6b5eebfa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2a6cf9d239e3427ead",
+    "url": "0.fa83264e.iframe.bundle.js"
+  },
+  {
+    "revision": "66622912cbd801473440",
+    "url": "4.7e95f215.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.7e95f215.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "029b4df223a8ad22a01f",
+    "url": "5.ff6d3c48.iframe.bundle.js"
+  },
+  {
+    "revision": "0d41f16dbcc99692ef09",
+    "url": "6.b8ed1d7f.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.b8ed1d7f.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "250e201117de4855cc0b",
+    "url": "7.124f9e1d.iframe.bundle.js"
+  },
+  {
+    "revision": "47eed085ca18f09fc5bf37b5a00fd27c",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "7a52414888dbceaf2f50",
+    "url": "main.ee400f89.iframe.bundle.js"
+  },
+  {
+    "revision": "d5278599900bec7937dd",
+    "url": "runtime~main.6a618017.iframe.bundle.js"
+  },
+  {
+    "revision": "7a52414888dbceaf2f50",
+    "url": "static/css/main.a8d1971e.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "f5d111e468a312f9d552",
+    "url": "vendors~main.33cf1145.iframe.bundle.js"
+  },
+  {
+    "revision": "bdc18148033fe83705324775c3087455",
+    "url": "vendors~main.33cf1145.iframe.bundle.js.LICENSE.txt"
+  }
+]);
