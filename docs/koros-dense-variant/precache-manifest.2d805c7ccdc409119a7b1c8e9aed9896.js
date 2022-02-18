120c2963e83de57713e1efcc5395082608150f1d
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-02-18 15:32:19 +0200

    Add koros dense demo

---
+++ docs/koros-dense-variant/precache-manifest.2d805c7ccdc409119a7b1c8e9aed9896.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e362399355c3df14a8d7",
+    "url": "0.e192af9a.iframe.bundle.js"
+  },
+  {
+    "revision": "cd29ff53673056576d81",
+    "url": "4.eb80befc.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.eb80befc.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "80225a67a8c994a4d709",
+    "url": "5.c1bb7f26.iframe.bundle.js"
+  },
+  {
+    "revision": "55f9d95e4f3bafe16bca",
+    "url": "6.0ac91277.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.0ac91277.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "9096f9676c03d045d907",
+    "url": "7.fb9f8aec.iframe.bundle.js"
+  },
+  {
+    "revision": "dca0af5ac6039dd45eb32e226ed55648",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "d34c0f038cc4b7cc3564",
+    "url": "main.78f254f0.iframe.bundle.js"
+  },
+  {
+    "revision": "af6c8878dc79e0583761",
+    "url": "runtime~main.14c5a33b.iframe.bundle.js"
+  },
+  {
+    "revision": "d34c0f038cc4b7cc3564",
+    "url": "static/css/main.a8d1971e.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "cbbf7b120643b7456ae5",
+    "url": "vendors~main.02480f0d.iframe.bundle.js"
+  },
+  {
+    "revision": "ad769daa05445f1ec9a3e6e1820e6338",
+    "url": "vendors~main.02480f0d.iframe.bundle.js.LICENSE.txt"
+  }
+]);
