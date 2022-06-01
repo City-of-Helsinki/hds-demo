d1eb8cc59372d442f919c65676584e97dd4a547d
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-01 14:28:46 +0300

    Add new navigation shelf demo

---
+++ docs/navigation-shelf/precache-manifest.ff4e4e36ead6381c8b579f76fb2aa9c4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087926eb7f58c867b003",
+    "url": "0.c01ad12b.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "0.c01ad12b.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "23102fc40892db3d1128",
+    "url": "1.5d94f8c2.iframe.bundle.js"
+  },
+  {
+    "revision": "9abe3f98a30f7fe392cb",
+    "url": "2.35015ff4.iframe.bundle.js"
+  },
+  {
+    "revision": "630cc0b47de16fa736e5",
+    "url": "6.4a00cfa4.iframe.bundle.js"
+  },
+  {
+    "revision": "3fe3e966a0ba7a992e41",
+    "url": "7.652d43f6.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "7.652d43f6.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee065909f2c48f62e63",
+    "url": "8.28e1dcf1.iframe.bundle.js"
+  },
+  {
+    "revision": "000142c52ee78c94c4fd",
+    "url": "9.1c51b29a.iframe.bundle.js"
+  },
+  {
+    "revision": "55ebdedbd4ee3a9e93f7110c5f0574d0",
+    "url": "9.1c51b29a.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "c293e4a360b0f04c589b69de0aae9e6e",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "6f748523fb1801ff07fa",
+    "url": "main.da15fca5.iframe.bundle.js"
+  },
+  {
+    "revision": "9937593c6c31143478e3",
+    "url": "runtime~main.357dc238.iframe.bundle.js"
+  },
+  {
+    "revision": "6f748523fb1801ff07fa",
+    "url": "static/css/main.44666598.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "3099e6cf5e6780eb4d3b",
+    "url": "vendors~main.efb9bbe3.iframe.bundle.js"
+  },
+  {
+    "revision": "fc1f75c91625343fbf9d9b2ad5f626fb",
+    "url": "vendors~main.efb9bbe3.iframe.bundle.js.LICENSE.txt"
+  }
+]);
